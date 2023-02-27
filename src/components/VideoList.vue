d4dfe492f2bf0a92d0f9a2da5654ed4de6acd8db
zurikelass <zuragogberashvili93@gmail.com>
2023-02-27 05:38:11 +0400

    სერჩი ვერ ავამუშავე

---
+++ src/components/VideoList.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  props: {
+    videos: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+<template>
+  <ul>
+    <li v-for="video in videos" :key="video.id">
+      <h2>{{ video.title }}</h2>
+      <p>{{ video.description }}</p>
+      <video :src="video.url" controls></video>
+    </li>
+  </ul>
+</template>
---
+++ src/components/navigation/Search.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+      searchQuery: "",
+    };
+  },
+  watch: {
+    searchQuery() {
+      this.$emit("search", this.searchQuery.trim());
+    },
+  },
+};
+</script>
+
+<template>
+  <input type="text" v-model="searchQuery" placeholder="Search..." />
+</template>
