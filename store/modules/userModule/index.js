9d55c4fbace91357eeca0735019d11cdaae94d96
zurikelass <zuragogberashvili93@gmail.com>
2023-03-02 07:25:53 +0400

    user module

--- store/modules/userModule/index.js
+++ store/modules/userModule/index.js
@@ -1,42 +1,40 @@
 const user = {
-    namespaced : true,
-    state(){
-        return {
-            authanticated: null,
-            users: [
-             {
-                id: 1,
-                email: 'zack@mail.com',
-                password: 'zack123',
-             }   
-            ]
-        }
- 
-   },
-   getters: {
-      verifiedUser(state){
-        return state.authanticated
+  namespaced: true,
+  state() {
+    return {
+      authanticated: null,
+      users: [
+        {
+          id: 1,
+          email: "zack@mail.com",
+          password: "zack123",
+        },
+      ],
+    };
+  },
+  getters: {
+    verifiedUser(state) {
+      return state.authanticated;
+    },
+  },
+  mutations: {
+    VERIFY(state, payload) {
+      let user = state.user.filter((val) => val.email === payload.email);
+      if (user.password === payload.password) {
+        state.verified = user;
       }
-   },
-   mutations: {
-          VERIFY(state,payload){
-            let user =state.user.filter(val => val.email === payload.email)
-            if(user.password === payload.password){
-                state.verified=user
-            }
-          },
-          REGISTER_NEW_USER(state,payload){
-            state.user.push(payload)
-          }
-   },
-   actions: {
-      verify({commit},payload ){
-        commit(VERIFY, payload)
-       },
-           register({commit},payload ){
-        commit("REGISTER_NEW_USER", payload)
-        commit("VERIFY", payload)
-       },
-   },
-
-}
+    },
+    REGISTER_NEW_USER(state, payload) {
+      state.user.push(payload);
+    },
+  },
+  actions: {
+    verify({ commit }, payload) {
+      commit(VERIFY, payload);
+    },
+    register({ commit }, payload) {
+      commit("REGISTER_NEW_USER", payload);
+      commit("VERIFY", payload);
+    },
+  },
+};
