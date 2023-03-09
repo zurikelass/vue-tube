b993ee9d95891564ef3e7c87fb3ea172687b44f3
zurikelass <zuragogberashvili93@gmail.com>
2023-03-09 05:41:40 +0400

    added register and login

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,3 +1,32 @@
 
 
 
+
+<script>
+export default {
+  name: "UserList",
+  data() {
+    return {
+      users: []
+    };
+  },
+  mounted() {
+    fetch("https://items.magischer.de/api/users")
+      .then(response => response.json())
+      .then(data => {
+        this.users = data;
+      });
+  }
+};
+</script>
+
+<template>
+  <div>
+    <h1>Users:</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.name }}
+      </li>
+    </ul>
+  </div>
+</template>
