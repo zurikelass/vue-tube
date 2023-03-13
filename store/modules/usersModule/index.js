83662a54683860f0e784f6fb4cfdeece0a46704d
zurikelass <zuragogberashvili93@gmail.com>
2023-03-13 05:15:42 +0400

    category

--- store/modules/usersModule/index.js
+++ store/modules/usersModule/index.js
@@ -6,12 +6,20 @@ const userModule = {
     };
   },
   getters: {
-    
+    info(state) {
+      return state.users;
+    },
+    getApiUrl: (state) => {
+      return 'https://items.magischer.de/api/users';
+    },
   },
   mutations: {
+    setUsers(state, users) {
+      state.users = users;
+    },
   },
-  actions: {
-}
+  actions: {},
+};
 
-}
+export default userModule;
 
